f3db94a88b9966e34d6c4dc602914b3d0b3d98c1
kamateshreyas <135574233+kamateshreyas@users.noreply.github.com>
2026-04-04 18:56:49 +0530

    Refactor QR model to use module exports

--- Backend/models/qrModel.js
+++ Backend/models/qrModel.js
@@ -1,12 +1,16 @@
 const db = require("../../database/db");
 
 // Insert QR
-exports.createQR = (data, callback) => {
+const createQR = (data, callback) => {
   const sql = "INSERT INTO qr_codes (type, content, qr_code) VALUES (?, ?, ?)";
   db.query(sql, [data.type, data.content, data.qr_code], callback);
 };
 
 // Get all QR
-exports.getAllQR = (callback) => {
+const getAllQR = (callback) => {
   db.query("SELECT * FROM qr_codes ORDER BY created_at DESC", callback);
+};
+module.exports = {
+    createQR,
+    getAllQR
 };
