b0b98761ac8b8bc8240bbc8f158656a328f08a6b
Shreyas Kamate <shreyaskamate@Shreyass-MacBook-Air.local>
2026-04-05 20:59:52 +0530

    Fixed CloudinaryStorage import

--- Backend/middleware/upload.js
+++ Backend/middleware/upload.js
@@ -1,21 +1,24 @@
 const multer = require("multer");
-const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
+// ✅ Cloudinary config
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_API_KEY,
   api_secret: process.env.CLOUD_API_SECRET,
 });
 
+// ✅ Storage config
 const storage = new CloudinaryStorage({
-  cloudinary,
+  cloudinary: cloudinary,
   params: {
     folder: "qr-files",
     resource_type: "auto",
   },
 });
 
+// ✅ Upload middleware
 const upload = multer({ storage });
 
 module.exports = upload;
