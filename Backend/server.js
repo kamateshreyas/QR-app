105943ac32444d1cb0aa1a7ccdb32fd4e02a26db
Shreyas Kamate <shreyaskamate@Shreyass-MacBook-Air.local>
2026-04-06 16:21:25 +0530

    Fix on server

--- Backend/server.js
+++ Backend/server.js
@@ -10,18 +10,20 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5500",
-    "https://https://qr-app-chi-ten.vercel.app"
+    "https://qr-app-chi-ten.vercel.app"
   ],
   methods: ["GET", "POST"],
   credentials: true
 }));
+
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   next();
 });
-app.options("*", cors());
+
+app.options(/.*/, cors());
 // JSON parsing
 app.use(express.json());
 
